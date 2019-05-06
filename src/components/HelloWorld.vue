3f135cbe2889805a47ac621a2e78e8546fcf952f
zhumingjie <zmj@enn.com>
2019-05-06 15:03:42 +0800

    Vue练习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,54 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <div>{{inputvalue}}</div>
+    <input type="text" name="" id="" v-model="inputvalue">
+    <p>{{objvalue.name}}</p>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
+      <li v-for="item in list">{{item}}</li>
     </ul>
-    <h3>Essential Links</h3>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li v-for="item in list1">{{item.title}}</li>
     </ul>
-    <h3>Ecosystem</h3>
+    <hr>
+    <br>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="item in list2">
+        {{item.cate}}
+        <ol>
+          <li v-for="i in item.lists">{{i.title}}</li>
+        </ol>
+      </li>
     </ul>
+    <hr>
+    <br>
+    <div v-bind:class="{'red':flag}">这是一个div</div>
+    <ul>
+      <li v-for="(item, key) in list" v-bind:class="{'red':key==0,'blue':key==1}">{{item}}</li>
+    </ul>
+    <hr>
+    <br>
+
+    <div class="box" v-bind:style="{'width':boxwidth}">
+    这是一个div
+    </div>
+    <hr>
+    <br>
+
+    <button @click="getMsg">获取</button>
+    <button @click="setMsg()">设置</button>
+    <hr>
+    <br>
+    <input type="text" ref="userinfo" />
+    <div ref="box">box</div>
+    <button @click="getInputValue()">获取input值</button>
+    <hr>
+    <br>
+    <button data-aid="123" @click="eventFn($event)">事件获取自定义属性值</button>
   </div>
 </template>
 
@@ -37,6 +58,50 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  private inputvalue: string = '你好';
+  private objvalue: object = { name: '张三' };
+  private list = [1, 2, 3];
+  private list1 = [
+    { title: 111 },
+    { title: 222 },
+    { title: 333 }
+  ];
+  private list2 = [
+    {
+      cate: '国内新闻',
+      lists: [
+        { title: 'aaa' },
+        { title: 'bbb' },
+        { title: 'ccc' }
+      ]
+    },
+    {
+      cate: '国外新闻',
+      lists: [
+        { title: 'AAA' },
+        { title: 'BBB' },
+        { title: 'CCC' }
+      ]
+    }
+  ];
+  private flag: boolean = true;
+  private boxwidth: string = '300px';
+
+  private getMsg() {
+    alert(this.inputvalue);
+  };
+  private setMsg() {
+    this.inputvalue = '我是vue'
+  };
+  private getInputValue() {
+    alert(this.$refs.userinfo.value);
+    this.$refs.box.style.background = "red";
+  };
+  private eventFn(e) {
+    console.log(e);
+    alert(e.srcElement.dataset.aid);
+  }
 }
 </script>
 
@@ -55,5 +120,16 @@ li {
 }
 a {
   color: #42b983;
+}
+.red{
+  color: red;
+}
+.blue{
+  color:blue;
+}
+.box{
+  width: 100px;
+  height: 100px;
+  background: rgba(1,1,1,0.3);
 }
 </style>
