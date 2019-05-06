d774299d796b5ec41c29b716504f6b3f20b5b320
zhumingjie <zmj@enn.com>
2019-05-06 18:06:21 +0800

    增加待办事项路由

---
+++ src/views/TODOList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="todolist">
+        <p>待办事项</p>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue} from 'vue-property-decorator'
+export default class TODOList extends Vue{
+    
+}
+</script>
+
